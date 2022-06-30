e2d76c2460ae5ad11c4677ddf45444f3d1e04b20
natefoxr <nate@foxr.xyz>
2022-06-29 23:30:13 -0700

    Periodic commit for TIL app - added tailwind CSS and Login Logout

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,9 @@
+from pathlib import PosixPath
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
