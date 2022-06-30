f6bec11cd265c9395061f4ae43245a703bddae77
natefoxr <nate@foxr.xyz>
2022-06-30 02:12:21 -0700

    Created Static files and post creation popup

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
@@ -9,3 +11,25 @@ class HomePage(ListView):
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by("-id")[0:30]
+
+class PostDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = "feed/detail.html"
+    model = Post
+    context_object_name = "post"
+
+class CreateNewPostView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "feed/create.html"
+    fields = ['text']
+    success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
--- til/settings.py
+++ til/settings.py
@@ -152,6 +152,12 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend"
 )
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
